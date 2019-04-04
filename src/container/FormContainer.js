bbffdd7f21557b49c36cf33c59b8eb64ad2c8dbc
Carmen Perezchica <crperezchica@gmail.com>
2019-04-04 14:58:12 -0700

    finished store, added props to form, reducer needs work

--- src/container/FormContainer.js
+++ src/container/FormContainer.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
 import Form from '../components/form/Form';
 
-export default class FormContainer extends React.PureComponent {
+export default class FormContainer extends PureComponent {
   render() {
     return (
       <Form />
